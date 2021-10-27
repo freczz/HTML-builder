64cc9bc70db8604cc1fe816b0acb8afe7919499c
Alexey <89932423+freczz@users.noreply.github.com>
2021-10-27 19:28:20 +0300

    feat: make 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
-const chalk = require('chalk')
+const chalk = require('chalk');
+
 let stream = fs.createReadStream('01-read-file/text.txt', { encoding: 'utf-8' });
+
 stream.on('data', (data) => { console.log(chalk.green(data)) })
