46faaa16152e59bb56de6a594030b964d0414349
Alexey <89932423+freczz@users.noreply.github.com>
2021-11-06 19:13:07 +0300

    feat: make 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs')
+const path = require('path');
+const chalk = require('chalk')
 
+async function copyFiles() {
+   const items = await fs.promises.readdir(__dirname, { withFileTypes: true })
+   for (item of items) {
+      if (!item.isFile() && item.name != path.join('files-copy')) {
+         fs.mkdir('04-copy-directory/files-copy', err => {
+            if (err) {
+               console.log(chalk.green('Папка успешно актуализирована'))
+               return
+            }
+            console.log(chalk.green('Папка успешно создана и актуализирована'))
+         })
+      }
+   }
+
+   const filesCopy = await fs.promises.readdir(path.join(__dirname, 'files-copy'))
+   for (fileCopy of filesCopy) {
+      fs.unlink(path.join(__dirname, `files-copy/${fileCopy}`), err => { if (err) throw err })
+   }
+
+   const files = await fs.promises.readdir(path.join(__dirname, 'files'))
+   for (file of files) {
+      fs.copyFile(path.join(__dirname, `files/${file}`), path.join(__dirname, `files-copy/${file}`), err => { if (err) throw err })
+   }
+}
+copyFiles()
