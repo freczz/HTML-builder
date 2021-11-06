8800caca90b383662ec163fa7af728f9f65d83f6
Alexey <89932423+freczz@users.noreply.github.com>
2021-11-06 23:07:01 +0300

    feat: make 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path');
+const chalk = require('chalk')
+
+async function merge() {
+   const items = await fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+   for (item of items) {
+      let stream = fs.createReadStream(path.join(__dirname, `styles/${item.name}`), { encoding: 'utf-8' })
+
+      fs.writeFile(path.join(__dirname, 'project-dist/bundle.css'), '', 'utf-8', (err) => { if (err) throw err })
+
+      fs.truncate(path.join(__dirname, 'project-dist/bundle.css'), 0, (err) => { if (err) throw err; })
+
+      if (path.extname(item.name) == '.css') {
+         stream.on('data', (data) => {
+            fs.appendFile(path.join(__dirname, 'project-dist/bundle.css'), data, (err) => { if (err) throw err })
+         })
+      }
+   }
+   console.log(chalk.green('Файл создан'))
+}
+merge()
