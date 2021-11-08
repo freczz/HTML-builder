cfc908b44a3c1c34cb6c5828e3009f15f781f051
Alexey <89932423+freczz@users.noreply.github.com>
2021-11-08 18:02:57 +0300

    feat: make 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const path = require('path')
-const chalk = require('chalk')
 
 async function getFiles() {
    const items = await fs.promises.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true });
@@ -8,11 +7,11 @@ async function getFiles() {
       if (item.isFile()) {
          const file = path.join(__dirname, `secret-folder/${item.name}`)
          await fs.stat(file, async (err, data) => {
-            console.log(chalk.green(`<${path.parse(file).name}>-<${path.parse(file).ext.slice(1)}>-<${Math.round(data.size / 1024)}kb>`))
-            console.log(chalk.blue('----------------------'))
+            console.log(`<${path.parse(file).name}>-<${path.parse(file).ext.slice(1)}>-<${Math.round(data.size / 1024)}kb>`)
+            console.log('----------------------')
          })
       }
    }
-   console.log(chalk.blue('Result:\n----------------------'))
+   console.log('Result:\n----------------------')
 }
 getFiles()
