e5449bb1fdde652aa38ccbb1139cc881d1180a42
Alexey <89932423+freczz@users.noreply.github.com>
2021-11-08 18:01:58 +0300

    feat: make 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,27 +1,26 @@
-const fs = require('fs');
-const chalk = require('chalk');
-const process = require('process');
-const readline = require('readline');
+const fs = require('fs')
+const process = require('process')
+const readline = require('readline')
 
 const rl = readline.createInterface({
    input: process.stdin,
    output: process.stdout,
-});
+})
 
 let stream;
 
-console.log(chalk.blue('Write yout text: '));
+console.log('Write yout text: ')
 
 rl.on('line', (data) => {
    if (!stream && data !== 'exit') stream = fs.createWriteStream('02-write-file/text.txt', { encoding: 'utf-8' })
    if (data === 'exit') {
-      goodBye();
-      process.exit();
+      goodBye()
+      process.exit()
    }
    stream.write(`${data} \n`);
    fs.createReadStream('02-write-file/text.txt', { encoding: 'utf-8' })
 });
 
-process.on('beforeExit', goodBye);
+process.on('beforeExit', goodBye)
 
-function goodBye() { console.log(chalk.green('See you next time!')) }
+function goodBye() { console.log('See you next time!') }
